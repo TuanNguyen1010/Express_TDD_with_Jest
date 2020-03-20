f8394446ae018c16d26286b147ca33899752f995
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-20 00:18:43 +0000

    Refeactor: req, res and next is refactored to be set before each test

--- tests/unit/todo.test.js
+++ tests/unit/todo.test.js
@@ -5,15 +5,19 @@ const toDoMocks = require('../mocks/toDoMock')
 
 ToDoModel.create = jest.fn()
 
+let req, res, next;
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+})
+
 describe('ToDoController.createToDo', () => {
   it('should have a createToDo function', () => {
     expect(typeof ToDoController.createToDo).toBe('function')
   })
   it('should call the todo model from controller', () => {
-    let req, res, next;
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null;
     req.body = toDoMocks;
     ToDoController.createToDo(req, res, next)
     expect(ToDoModel.create).toBeCalledWith(toDoMocks)
