b9d120bf3bbf9dad894a0d12e83487e5fa61c091
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-30 02:29:08 +0100

    Test: integration testing for id routes

--- routes/todo.js
+++ routes/todo.js
@@ -5,5 +5,6 @@ const ToDoController = require('../controllers/todo')
 
 router.post('/', ToDoController.createToDo)
 router.get('/', ToDoController.getToDo)
+router.get('/:todoId', ToDoController.getToDoById)
 
 module.exports = router
--- tests/integration/todo.test.js
+++ tests/integration/todo.test.js
@@ -1,24 +1,31 @@
 const request = require("supertest");
 const app = require('../../app.js');
 const createToDoMock = require("../mocks/createToDoMock.json");
-const endpointUrl = "/todos";
+const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
-
   it("GET" + endpointUrl, async () => {
     const response = await request(app)
       .get(endpointUrl)
     expect(response.statusCode).toBe(200)
     expect(Array.isArray(response.body)).toBeTruthy()
+    firstTodo = response.body[0]
+  })
+  it("GET" + endpointUrl + ":todoId", async () => {
+    const response = await request(app)
+    .get(endpointUrl + firstTodo._id)
+    expect(response.statusCode).toBe(200)
+    expect(response.body.title).toBe(firstTodo.title)
+    expect(response.body.done).toBe(firstTodo.done)
   })
 
   it("POST " + endpointUrl, async () => {
     const response = await request(app)
-        .post(endpointUrl)
-        .send(createToDoMock);
-      expect(response.statusCode).toBe(201);
-      expect(response.body.title).toBe(createToDoMock.title);
-      expect(response.body.done).toBe(createToDoMock.done);
+      .post(endpointUrl)
+      .send(createToDoMock);
+    expect(response.statusCode).toBe(201);
+    expect(response.body.title).toBe(createToDoMock.title);
+    expect(response.body.done).toBe(createToDoMock.done);
   })
 
   it('should return error 500 on malformed data with POST' + endpointUrl,
